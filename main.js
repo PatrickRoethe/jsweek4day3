69be08ee1bbe9efb4ceec371aaba1569fc2a469d
PatrickRoethe <146090971+PatrickRoethe@users.noreply.github.com>
2024-02-01 12:38:54 +0100

    update index and main
    added todo list and updated main.js to get data from API

--- main.js
+++ main.js
@@ -1,1 +1,31 @@
 // console.log("HEEEEY");
+
+// Assume you have an HTML file with an element with the id "todo-list"
+let todoListElement = document.getElementById("todo-list");
+
+// Function to fetch todo items from an API
+function fetchTodos() {
+  return fetch("https://jsonplaceholder.typicode.com/todos").then((response) =>
+    response.json()
+  );
+}
+
+// Function to display todos in the HTML
+function displayTodos(todos) {
+  // Clear existing todo list
+  todoListElement.innerHTML = "";
+
+  // Loop through todos and create list items
+  todos.forEach((todo) => {
+    let listItem = document.createElement("li");
+    listItem.textContent = todo.title;
+    todoListElement.appendChild(listItem);
+  });
+}
+
+// Fetch todos and display them when the page loads
+document.addEventListener("DOMContentLoaded", function () {
+  fetchTodos()
+    .then((todos) => displayTodos(todos))
+    .catch((error) => console.error(error));
+});
